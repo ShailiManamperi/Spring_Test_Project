17e1f644d9aacd14d84715347157a37c0afdc30a
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-08-05 14:59:12 +0530

    update order class

--- src/main/java/lk/ijse/gdse63/demo1/api/Order.java
+++ src/main/java/lk/ijse/gdse63/demo1/api/Order.java
@@ -1,4 +1,26 @@
 package lk.ijse.gdse63.demo1.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("order")
+
 public class Order {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public String saveOrderJSON(){
+        return "Saved Order JSON";
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE,produces = MediaType.APPLICATION_XML_VALUE)
+    public String saveOrderXML(){
+        return "Saved Order XML";
+    }
+
 }
