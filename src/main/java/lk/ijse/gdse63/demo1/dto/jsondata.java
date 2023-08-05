8446a72f5ff808e60655b6dd6a2889386336b125
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-08-05 14:59:35 +0530

    Add classes

---
+++ src/main/java/lk/ijse/gdse63/demo1/dto/jsondata.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse63.demo1.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+@Getter
+@Setter
+public class jsondata {
+    private String id;
+    private String name;
+    private String city;
+}
